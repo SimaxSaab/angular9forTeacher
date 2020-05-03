2e20ea5c0b2188db6e975351f6cd30102adf05d3
simax_saab <minoreat@gmail.com>
2020-05-03 19:00:30 +0300

    Finished logics structure of group options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular9forTeacher';
-  showOption = false;
+  title:string = 'angular9forTeacher';
+  signShowGroupOption:boolean = false;
+  signAddNewGroupOption:boolean = false;
+  signEditGroupOption:boolean = false;
+  signDeleteGroupOption:boolean = false;
+  captionGroupOption = "Опции";
+  captionAddNewGroupOption = "Добавить новую группу";
+  captionEditGroupOption = "Редактировать выбранную группу";
+  constructor(){
+    
+  }
+  showGroupOption(){
+    if(this.signShowGroupOption)
+      this.captionGroupOption = "\u00d7";
+    else
+      this.captionGroupOption = "Опции";
+  }
+  showAddNewGroupOption(){
+    if(this.signAddNewGroupOption)
+      this.captionAddNewGroupOption = "Создать";
+    else
+      this.captionAddNewGroupOption = "Добавить новую группу";
+  }
+  showEditGroupOption(){
+    if(this.signEditGroupOption)
+      this.captionEditGroupOption = "Применить";
+    else
+      this.captionEditGroupOption = "Редактировать выбранную группу";
+  }
 }
