d36b5c0c237f74598c7793ac772238d1bb7c679c
simax_saab <minoreat@gmail.com>
2020-05-06 19:06:01 +0300

    Form without validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title:string = 'angular9forTeacher';
+  groups = [{name:"ЭМС-00-1"},
+   {name:"ЭМС-00-2"}, 
+   {name:"ЭМС-00-3"}]; 
   signShowGroupOption:boolean = false;
   signAddNewGroupOption:boolean = false;
   signEditGroupOption:boolean = false;
@@ -14,6 +17,10 @@ export class AppComponent {
   captionGroupOption = "Опции";
   captionAddNewGroupOption = "Добавить новую группу";
   captionEditGroupOption = "Редактировать выбранную группу";
+  nameNewGroup:string ='';
+  nameEditGroup:string;
+  newNameEditGroup:string;
+  newN:string;
   constructor(){
     
   }
@@ -23,16 +30,37 @@ export class AppComponent {
     else
       this.captionGroupOption = "Опции";
   }
+  addNewGroup() {
+    if(this.signAddNewGroupOption) this.groups.push({name:this.nameNewGroup});
+  }
   showAddNewGroupOption(){
     if(this.signAddNewGroupOption)
       this.captionAddNewGroupOption = "Создать";
     else
       this.captionAddNewGroupOption = "Добавить новую группу";
   }
+  editGroup() {
+    if(this.signEditGroupOption) {
+      this.groups.forEach((item, idx)=>{
+        if(item.name == this.nameEditGroup) {
+          item.name = this.newNameEditGroup;
+        }
+      });
+    }
+  }
   showEditGroupOption(){
-    if(this.signEditGroupOption)
+    if(this.signEditGroupOption) {
       this.captionEditGroupOption = "Применить";
+      this.newNameEditGroup = this.nameEditGroup;
+    }
     else
       this.captionEditGroupOption = "Редактировать выбранную группу";
+  }
+  deleteGroup(){
+    this.groups.forEach((item, idx, arr)=>{
+      if(item.name == this.nameEditGroup) {
+        arr.splice(idx,1);
+      }
+    });
   }
 }
