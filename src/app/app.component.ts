5bcbef249448141800355f41b14f752c75b00488
SimaxSaab <minorea@yandex.ru>
2020-08-14 22:28:58 +0300

    First dev commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,66 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators, FormArray } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title:string = 'angular9forTeacher';
-  groups = [{name:"ЭМС-00-1"},
-   {name:"ЭМС-00-2"}, 
-   {name:"ЭМС-00-3"}]; 
-  signShowGroupOption:boolean = false;
-  signAddNewGroupOption:boolean = false;
-  signEditGroupOption:boolean = false;
-  signDeleteGroupOption:boolean = false;
+export class AppComponent implements OnInit {
+  mainForm: FormGroup;
+  form: FormGroup;
+  formEdit: FormGroup;
+  ngOnInit() {
+    this.mainForm = new FormGroup({
+      mainFormSelect: new FormControl("", []),
+    });
+    this.form = new FormGroup({
+      name: new FormControl("", [
+        Validators.minLength(7),
+        Validators.maxLength(8),
+        Validators.pattern(RegExp("[А-Я]{2,3}-[0-9]{2}-[0-9]")),
+      ]),
+    });
+    this.formEdit = new FormGroup({
+      name1: new FormArray([]),
+    });
+  }
+
+  title: string = "angular9forTeacher";
+  groups = [{ name: "ЭМС-00-1" }, { name: "ЭМС-00-2" }, { name: "ЭМС-00-3" }];
+  signShowGroupOption: boolean = false;
+  signAddNewGroupOption: boolean = false;
+  signEditGroupOption: boolean = false;
+  signDeleteGroupOption: boolean = false;
   captionGroupOption = "Опции";
-  captionAddNewGroupOption = "Добавить новую группу";
   captionEditGroupOption = "Редактировать выбранную группу";
-  nameNewGroup:string ='';
-  nameEditGroup:string;
-  newNameEditGroup:string;
-  newN:string;
-  constructor(){
-    
-  }
-  showGroupOption(){
-    if(this.signShowGroupOption)
+  newNameEditGroup: string;
+  newN: string;
+  constructor() {}
+  showGroupOption() {
+    if (this.signShowGroupOption) {
       this.captionGroupOption = "\u00d7";
-    else
-      this.captionGroupOption = "Опции";
+    } else this.captionGroupOption = "Опции";
   }
-  addNewGroup() {
-    if(this.signAddNewGroupOption) this.groups.push({name:this.nameNewGroup});
+  addNewGroup(ev) {
+    // if (this.signAddNewGroupOption)
+    ev.preventDefault();
+    console.log(1);
+      this.groups.push({ name: this.form.get("name").value });
+    this.resetAddForm();
   }
-  showAddNewGroupOption(){
-    if(this.signAddNewGroupOption)
-      this.captionAddNewGroupOption = "Создать";
-    else
-      this.captionAddNewGroupOption = "Добавить новую группу";
+
+  addEditGroup() {
+    //console.log(this.formEdit);this.mainForm.get("mainFormSelect").value
+    const control = new FormControl( this.mainForm.get("mainFormSelect").value , [
+      Validators.required,
+      Validators.minLength(7),
+      Validators.maxLength(8),
+      Validators.pattern(RegExp("[А-Я]{2,3}-[0-9]{2}-[0-9]")),
+    ]);
+    (this.formEdit.get("name1") as FormArray).push(control);
+    console.log(this.formEdit.get("name1")["controls"]);
   }
+
   editGroup() {
-    if(this.signEditGroupOption) {
-      this.groups.forEach((item, idx)=>{
-        if(item.name == this.nameEditGroup) {
-          item.name = this.newNameEditGroup;
+    if (this.signEditGroupOption) {
+      this.groups.forEach((item, idx) => {
+        if (item.name == this.mainForm.get("mainFormSelect").value) {
+          console.log(1);
+          item.name = this.formEdit.get("name1")["controls"][0].value;
         }
       });
     }
+    this.deleteEditInput();
   }
-  showEditGroupOption(){
-    if(this.signEditGroupOption) {
-      this.captionEditGroupOption = "Применить";
-      this.newNameEditGroup = this.nameEditGroup;
-    }
-    else
-      this.captionEditGroupOption = "Редактировать выбранную группу";
+
+  deleteEditInput() {
+    console.log(this.formEdit.get("name1")["controls"][0]);
+    (this.formEdit.get("name1") as FormArray).removeAt(0);
   }
-  deleteGroup(){
-    this.groups.forEach((item, idx, arr)=>{
-      if(item.name == this.nameEditGroup) {
-        arr.splice(idx,1);
+
+  deleteGroup() {
+    this.groups.forEach((item, idx, arr) => {
+      if (item.name == this.mainForm.get("mainFormSelect").value) { 
+        arr.splice(idx, 1);
       }
     });
+  }
+  resetAddForm() {
+    this.form.reset();
+  }
+  resetMainForm() {
+    this.mainForm.reset();
   }
 }
